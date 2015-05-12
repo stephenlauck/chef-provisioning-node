931988f5ed5b0cb8fcfccbd13d3b8ea46a55188d
Stephen Lauck <stephen.lauck@gmail.com>
2015-05-12 15:27:19 -0400

    cross platform install of delivery-cli

--- test/fixtures/cookbooks/test/recipes/delivery-cli.rb
+++ test/fixtures/cookbooks/test/recipes/delivery-cli.rb
@@ -1,7 +1,18 @@
-execute 'installing-delivery-cli' do
-  command <<-EOF
-curl -o delivery-cli.rpm https://s3.amazonaws.com/delivery-packages/cli/delivery-cli-20150408004719-1.x86_64.rpm
-sudo yum install delivery-cli.rpm -y
-  EOF
-  not_if('which delivery')
+case node['platform_family']
+when 'debian'
+  execute 'installing-delivery-cli' do
+    command <<-EOF
+      curl https://packagecloud.io/install/repositories/chef/current/script.deb | sudo bash
+      sudo apt-get install delivery-cli
+    EOF
+    not_if('which delivery')
+  end
+when 'rhel'
+  execute 'installing-delivery-cli' do
+    command <<-EOF
+      curl -o delivery-cli.rpm https://s3.amazonaws.com/delivery-packages/cli/delivery-cli-20150408004719-1.x86_64.rpm
+      sudo yum install delivery-cli.rpm -y
+    EOF
+    not_if('which delivery')
+  end
 end
