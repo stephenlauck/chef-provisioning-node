bd32eb6d4969e2884e10e72b7f303ed26f4df89d
Stephen Lauck <stephen.lauck@gmail.com>
2015-05-12 16:06:20 -0400

    setup .ssh/config to use key for github and no strict host checking

--- test/fixtures/cookbooks/test/recipes/setup_ssh_keys.rb
+++ test/fixtures/cookbooks/test/recipes/setup_ssh_keys.rb
@@ -20,3 +20,25 @@ cookbook_file '/home/vagrant/.ssh/authorized_keys' do
   mode '0600'
   source 'insecure_public_key'
 end
+
+execute 'create ssh keys for github' do
+  command <<-EOF
+    ssh-keygen -t rsa -N '' -b 2048 -f /home/vagrant/.ssh/github_key
+  EOF
+  user 'vagrant'
+  group 'vagrant'
+  not_if do ::File.exists?('/home/vagrant/.ssh/github_key') end
+end
+
+file "/home/vagrant/.ssh/config" do 
+  content <<-EOD
+    HostName github.com
+    User git
+    IdentityFile ~/.ssh/github_key
+    StrictHostKeyChecking no 
+  EOD
+  owner 'vagrant'
+  group 'vagrant'
+  mode '0600'
+  not_if do ::File.exists?('/home/vagrant/.ssh/config') end
+end
