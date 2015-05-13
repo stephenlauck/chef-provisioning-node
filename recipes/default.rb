fea2221773d583b0e0ca631afc01bf4778bd1477
Stephen Lauck <stephen.lauck@gmail.com>
2015-05-13 13:50:50 -0400

    fix recipe location

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,10 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
+packagecloud_repo 'chef/current' do
+    type value_for_platform_family(debian: 'deb', rhel: 'rpm')
+end
+
 case node['platform_family']
 when 'debian'
 	include_recipe 'apt'
@@ -13,10 +17,8 @@ end
   include_recipe ir
 end
 
-chef_dk 'my_chef_dk' do
-    version 'latest'
-    global_shell_init true
-    action :install
+package 'chefdk' do
+  action :upgrade
 end
 
 chef_gem 'knife-push'
