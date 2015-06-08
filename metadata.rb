7c993f61a896f6aaa9d9d99fb2b9fa330fceab55
Bakh Inamov <b@chef.io>
2015-06-07 17:02:01 -0700

    version bump + rubocop

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-name             'chef-provisioning-node'
-maintainer       'The Authors'
+name 'chef-provisioning-node'
+maintainer 'The Authors'
 maintainer_email 'you@example.com'
-license          'all_rights'
-description      'Installs/Configures chef-provisioning-node'
+license 'all_rights'
+description 'Installs/Configures chef-provisioning-node'
 long_description 'Installs/Configures chef-provisioning-node'
-version          '0.1.1'
+version '0.1.2'
 
 %w(apt build-essential git packagecloud chef-dk).each do |cb|
   depends cb
